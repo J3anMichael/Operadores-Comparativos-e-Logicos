65c30bf1705517e7bb60dd2b87eb6a86af32ea7c
Jean Michael <jeanmmclaudino@gmail.com>
2021-12-08 22:37:34 -0300

    Operadores logicos

--- Operadores Comparativos e Logicos/Program.cs
+++ Operadores Comparativos e Logicos/Program.cs
@@ -15,35 +15,58 @@ namespace Operadores
             // == IGUAL
             // != DIFERENTE
 
-            int a = 10;
-            bool c1 =  a < 10;  // bool VARIAVEL(TRUE OR FALSE)
-            bool c2 = a < 20;
-            bool c3 = a > 10;
-            bool c4 = a > 5;
+            //int a = 10;
+            //bool c1 =  a < 10;  // bool VARIAVEL(TRUE OR FALSE)
+            //bool c2 = a < 20;
+            //bool c3 = a > 10;
+            //bool c4 = a > 5;
 
 
 
 
 
-            Console.WriteLine(c1);
-            Console.WriteLine(c2);
-            Console.WriteLine(c3);
-            Console.WriteLine(c4);
+            //Console.WriteLine(c1);
+            //Console.WriteLine(c2);
+            //Console.WriteLine(c3);
+            //Console.WriteLine(c4);
 
-            bool c5 = a <= 15;
-            bool c6 = a >= 15;
-            bool c7 = a == 10;
-            bool c8 = a != 10;
-
-
+            //bool c5 = a <= 15;
+            //bool c6 = a >= 15;
+            //bool c7 = a == 10;
+            //bool c8 = a != 10;
 
 
-            Console.WriteLine(c5);
-            Console.WriteLine(c6);
-            Console.WriteLine(c7);
-            Console.WriteLine(c8);
 
 
+            //Console.WriteLine(c5);
+            //Console.WriteLine(c6);
+            //Console.WriteLine(c7);
+            //Console.WriteLine(c8);
+
+
+
+            // OPERADORES LOGICOS
+
+            // & = E
+            // || = OU
+            // ! NAO
+
+            bool c1 = 2 > 3 && 4 != 5;
+
+            Console.WriteLine(c1); // FALSE
+
+            bool c2 = 2 > 3 || 4 != 5; 
+
+            Console.WriteLine(c2); // TRUE
+
+            bool c3 = !(2 > 3) && 4 != 5;
+
+            Console.WriteLine(c3); // TRUE
+
+
+
+            bool c4 = 10 < 5; // FALSE
+            bool c5 = c1 || c2 && c3;
 
 
 
